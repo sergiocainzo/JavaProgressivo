1d3af83d8e460e6e7d4cf575c14787fc313ed848
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-07-04 17:04:44 -0300

    ex05
    Se ao invés de você colocar uma String(ou seja, um texto entre as aspas) dentro dos métodos de print colocar uma operação matemática, como 21+21, o que acontece? O que é exibido?

---
+++ ex05.java
@@ -0,0 +1,9 @@
+public class ex05 {
+    public static void main(String[] args) {
+        System.out.println(
+                "Se ao invés de você colocar uma String(ou seja, um texto entre as aspas) dentro dos métodos de print colocar uma operação matemática, como 21+21, o que acontece? O que é exibido?");
+        System.out.println(21 + 21);
+        System.out.println("É exibido a soma dos numeros 21+21 = 42");
+
+    }
+}
