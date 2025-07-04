76862b9e362d0ab98ffd553b0504265642d1b0f6
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-07-04 17:01:46 -0300

    ex04
    Escreva um programa que mostre a seguinte carinha:

---
+++ ex04.java
@@ -0,0 +1,12 @@
+public class ex04 {
+    public static void main(String[] args) {
+        System.out.println("Escreva um programa que mostre a seguinte carinha: ");
+        System.out.println("");
+        System.out.println("  +'''''+  ");
+        System.out.println("[ | O O | ]");
+        System.out.println("  |  ^  |  ");
+        System.out.println("  | '-' |  ");
+        System.out.println("  +-----+  ");
+
+    }
+}
