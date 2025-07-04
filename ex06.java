16c03e48118cedc042faada52ab8308f2158eb30
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-07-04 17:08:42 -0300

    ex06
    Faça um programa, em Java, que exiba a seguinte operação matemática:
    21*12 = resultado da multiplicação

---
+++ ex06.java
@@ -0,0 +1,6 @@
+public class ex06 {
+    public static void main(String[] args) {
+        System.out.println("Faça um programa, em Java, que exiba a seguinte operação matemática:\n21*12 = resultado da multiplicação");
+        System.out.println("21 * 12 = "+ (21*12));
+    }
+}
