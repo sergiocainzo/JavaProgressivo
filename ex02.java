ef11d3dadecc09d20987ee933383691ef76df08d
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-07-04 16:47:09 -0300

    Exercicio 2
    Refaça o exercício anterior, agora usando apenas uma vez o método println().

---
+++ ex02.java
@@ -0,0 +1,13 @@
+public class ex02 {
+    public static void main(String[] args) {
+        // Atributo
+        String nome = "Testador", endereco = "Rua do testador, nº 1", cpf = "123.456.789-10";
+
+        System.out.println("\nRefaça o exercício anterior, agora usando apenas uma vez o método println()");
+        System.out.println(
+                "Exercício anterior:\nFaça um programa em JAVA que exiba seu nome na primeira linha, seu endereço na segunda linha e seu CPF na terceira linha, usando três vezes o métodos println()");
+        System.out.println("");
+        System.out.printf("\nNome: %s\nEndereço: %s\nCPF: %s\n", nome, endereco, cpf);
+
+    }
+}
