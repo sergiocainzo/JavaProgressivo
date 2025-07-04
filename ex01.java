3e584ac04666d7454e539d9f1d3e0bcd11cf11f6
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-07-04 16:41:42 -0300

    Initial commit

---
+++ ex01.java
@@ -0,0 +1,14 @@
+public class ex01 {
+    public static void main(String[] args) {
+        System.out.println(
+                "Faça um programa em JAVA que exiba seu nome na primeira linha, seu endereço na segunda linha e seu CPF na terceira linha, usando três vezes o métodos println()");
+
+        // Atributos
+        String nome = "Testador", endereco = "Rua do teste, nº 1", cpf = "123.456.789-10";
+
+        System.out.println("");
+        System.out.println("Nome: " + nome);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("CPF: " + cpf);
+    }
+}
