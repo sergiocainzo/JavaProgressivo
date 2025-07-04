56762bfce318936a89802d15a7cb783132c2f33b
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-07-04 17:17:32 -0300

    ex07
    Faça um programa que exiba na tela a tabuada do 9:

---
+++ ex07.java
@@ -0,0 +1,19 @@
+public class ex07 {
+    public static void main(String[] args) {
+        // Atributos
+        
+        System.out.println("Faça um programa que exiba na tela a tabuada do 9:");
+        System.out.println("");
+        System.out.printf("9 x  1 =  %d\n",9*1);
+        System.out.printf("9 x  2 = %d\n",9*2);
+        System.out.printf("9 x  3 = %d\n",9*3);
+        System.out.printf("9 x  4 = %d\n",9*4);
+        System.out.printf("9 x  5 = %d\n",9*5);
+        System.out.printf("9 x  6 = %d\n",9*6);
+        System.out.printf("9 x  7 = %d\n",9*7);
+        System.out.printf("9 x  8 = %d\n",9*8);
+        System.out.printf("9 x  9 = %d\n",9*9);
+        System.out.printf("9 x 10 = %d\n",9*10);
+
+    }
+}
