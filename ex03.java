cbfc80419bc004698edd1dfedc59b6eb98002fdf
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-07-04 16:56:48 -0300

    ex03
    Escreva um software que exiba a seguinte mensagem na tela: JAVA

---
+++ ex03.java
@@ -0,0 +1,9 @@
+public class ex03 {
+    public static void main(String[] args) {
+        System.out.println("\nEscreva um software que exiba a seguinte mensagem na tela: JAVA");
+        System.out.println("    J     A     V     V     A      ||");
+        System.out.println("    J    A A     V   V     A A     ||");
+        System.out.println("J   J   A A A     V V     A A A    ||");
+        System.out.println(" JJJ   A     A     V     A     A   **");
+    }
+}
